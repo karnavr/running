edc7b2fb39458f695d335bce45a9cd5b6fb3c984
karnavr <kraval3@uwo.ca>
2024-01-18 21:39:33 -0500

    create pacing notebook

---
+++ pacing.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.19.29
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d8801dec-b673-11ee-0957-61b3cb05ec18
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═d8801dec-b673-11ee-0957-61b3cb05ec18
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
